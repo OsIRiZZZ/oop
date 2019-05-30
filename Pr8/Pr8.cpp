e43956eb1e8077e6bf6c403ee8ee985cf535c2bb
OsIRiZZZ <47858945+OsIRiZZZ@users.noreply.github.com>
2019-05-30 15:58:34 +0300

    Практика 8

---
+++ Pr8/Pr8.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ifstream inp("in.txt");
+	string input = "";
+	while (!inp.eof()) {
+		inp >> input;
+		cout << input << endl;
+	}
+	inp.close();
+	return 0;
+}
