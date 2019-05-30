2bef83e7a25a5447c8fc24192a5b2648ded206b3
OsIRiZZZ <47858945+OsIRiZZZ@users.noreply.github.com>
2019-05-30 14:47:41 +0300

    Практика 2

---
+++ Pr2/Pr2/Pr2.cpp
@@ -0,0 +1,37 @@
+﻿#include "pch.h"
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double a, b, n, m;
+	cout << "Input variables a and b: ";
+	cin >> a >> b;
+	if (a >= b)
+	{
+		n = pow(a - b, 1.0 / 3);
+	}
+	else
+		if (sin(a*b) == 0)
+		{
+			cout << "Error";
+			return 0;
+		}
+		else
+			n = pow(a, 2) + (a - b) / sin(a*b);
+	if (n < b)
+		if (b == 0 || pow((pow(sin(a), 2) - cos(n)), 2) < 0)
+		{
+			cout << "Error";
+			return 0;
+		}
+		else
+			m = (n + a) / (-b) + sqrt(pow(sin(a), 2) - cos(n));
+	if (n == b)
+		m = pow(b, 2) + tan(n*a);
+	if (n > b)
+		m = pow(b, 3) + n * pow(a, 2);
+	return 0;
+}
