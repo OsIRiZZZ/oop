c1506fb7e659f03ffafe5593b9ac918610642159
OsIRiZZZ <47858945+OsIRiZZZ@users.noreply.github.com>
2019-05-30 14:46:53 +0300

    Практика 1

---
+++ Pr1/Pr1/Pr1.cpp
@@ -0,0 +1,17 @@
+﻿// Pr1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string name;
+    cout << "What is your name: ";
+	cin >> name;
+	cout << "Hello " << name << endl;
+}
+
